d626776a96b850317f694e7c081cb2e5e2c94939
lfabris-mhpc <lfabris@sissa.it>
2020-10-24 00:37:20 +0200

    revamped scripts, hand-made

---
+++ Assignement1/work/serial.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=48
+#PBS -l walltime=04:00:00
+#PBS -q dssc
+#PBS -j oe
+#PBS -N serial
+
+workdir=${PBS_O_WORKDIR}
+cd ${workdir}
+
+module purge
+module load openmpi/4.0.3/gnu/9.3.0
+
+n=100000000
+for rep in {1..4}
+do
+	/usr/bin/time -f "elapsed: %e\\nuser: %U\\nsystem: %S" mpirun --mca btl '^openib' -np ${p} ../mpi_pi.x ${n}
+	(( n *= 10 ))
+done
+
+exit 0
---
+++ Assignement1/work/weak_w09.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=48
+#PBS -l walltime=00:20:00
+#PBS -q dssc
+#PBS -j oe
+#PBS -N strong_w09
+
+workdir=${PBS_O_WORKDIR}
+cd ${workdir}
+
+module purge
+module load openmpi/4.0.3/gnu/9.3.0
+
+nbase=1000000000
+for p in 4 8 12 16 20 24 28 32 36 40 44 48
+do
+	(( n = p * nbase ))
+	/usr/bin/time -f "elapsed: %e\\nuser: %U\\nsystem: %S" mpirun --mca btl '^openib' -np ${p} ../mpi_pi.x ${n}
+done
+
+exit 0
