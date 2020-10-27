636fb83e94e2de4f35998d0643ffcc286fc9b96b
lfabris-mhpc <lfabris@sissa.it>
2020-10-27 22:34:43 +0100

    plotting from experiments' csvs

--- Assignement1/create_plots.py
+++ Assignement1/create_plots.py
@@ -3,16 +3,17 @@ import os as os
 import glob as glob
 #import math as math
 import pandas as pd
-#import mathplotlib as plt
+import matplotlib.pyplot as plt
 
 def merge(d, prefix, suffix=".csv", usecols=[0, 4]):
 	ret = None
 	for fn in sorted(glob.glob(os.path.join(d, prefix + "*[0-9]" + suffix))):
 		f = os.path.basename(fn)
-		nbase = 10**int(f[len(prefix):-len(suffix)])
+		exps = f[len(prefix):-len(suffix)]
+		nbase = 10**int(exps)
 		ncol = f"{nbase:.0E}"
 
-		df = pd.read_csv(fn, header=None, names=["P", ncol], index_col=False, usecols=[0, 4], comment="#")
+		df = pd.read_csv(fn, header=None, names=["P", ncol], index_col=[0], usecols=[0, 4], comment="#")
 
 		#print(f"file: {fn}")
 		#print(df)
@@ -24,29 +25,66 @@ def merge(d, prefix, suffix=".csv", usecols=[0, 4]):
 	
 	return ret
 
+def plot(df, title, ylabel, yscale="linear"):
+    for c in df.columns:
+        plt.plot(df.index, df[c], label=f"{1}{c[2:]}")
+
+    plt.title(title)
+
+    plt.xlabel("P")
+    #plt.xscale("log")
+    plt.ylabel(ylabel)
+    plt.yscale(yscale)
+    plt.legend()
+ 
+    plt.show()
+
 def process(d, prefix, context, usecols=[0, 4]):
 	suffix = ""
 	process = merge(d, prefix, suffix+".csv", usecols)
-	print(context + "-process:")
-	print(process)
+	title = "process time"
+	plot(process, title, "time (s)", "log")
+
+	scalability = pd.DataFrame()
+	scalability[process.columns[0]] = process[process.columns[0]]
+	for c in process.columns:
+		scalability[c] = process[c][1] / process[c]
+	
+	title = "process time scalability"
+	plot(scalability, title, "scalability")
 
 	suffix = "-elapsed"
 	elapsed = merge(d, prefix, suffix+".csv", usecols)
-	print(context + "-elapsed:")
-	print(elapsed)
+	title = "elapsed time"
+	plot(elapsed, title, "time (s)", "log")
+	
+	scalability = pd.DataFrame()
+	scalability[elapsed.columns[0]] = elapsed[elapsed.columns[0]]
+	for c in elapsed.columns:
+		scalability[c] = elapsed[c][1] / elapsed[c]
+	
+	title = "elapsed time scalability"
+	plot(scalability, title, "scalability")
+	
+	suffix = "-system"
+	system = merge(d, prefix, suffix+".csv", usecols)
+	title = "system time"
+	plot(system, title, "time (s)", "log")
 
 	suffix = "-system"
 	system = merge(d, prefix, suffix+".csv", usecols)
-	system = system.div(system["P"], axis=0)
-	system["P"] = elapsed["P"]
-	print(context + "-system (scaled):")
-	print(system)
-
+	system = system.div(system.index, axis=0)
+	title = "system time (scaled)"
+	plot(system, title, "time (s)", "log")
+	
 	overhead = elapsed - process
-	overhead = overhead.div(elapsed["P"], axis=0)
-	overhead["P"] = elapsed["P"]
-	print(context + "-overhead (scaled):")
-	print(overhead)
+	title = "overhead time"
+	plot(overhead, title, "time (s)", "log")
+	
+	overhead = elapsed - process
+	overhead = overhead.div(overhead.index, axis=0)
+	title = "overhead time (scaled)"
+	plot(overhead, title, "time (s)", "log")
 
 d = "csvs"
 
