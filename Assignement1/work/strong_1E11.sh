0c417667bdd84201ea3a4b34e5ee8486abc94393
lfabris-mhpc <lfabris@sissa.it>
2020-10-27 11:15:32 +0100

    scripts for thin

---
+++ Assignement1/work/strong_1E11.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=24
+#PBS -l walltime=12:00:00
+#PBS -q thin
+#PBS -j oe
+#PBS -N strong_w11
+
+workdir=${PBS_O_WORKDIR}
+cd ${workdir}
+
+module purge
+module load openmpi/4.0.3/gnu/9.3.0
+
+nbase=100000000000
+for p in 1 4 8 12 16 20 24
+do
+	/usr/bin/time -f "elapsed: %e\\nuser: %U\\nsystem: %S" mpirun --mca btl '^openib' -np ${p} ../mpi_pi.x ${nbase}
+done
+
+exit 0
