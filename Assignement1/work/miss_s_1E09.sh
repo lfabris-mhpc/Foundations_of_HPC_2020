8e1ad077f24ab07e9038fd7662c9a4e4b6b8a7a4
lfabris-mhpc <lfabris@sissa.it>
2020-10-27 16:13:49 +0100

    scripts for missed steps

---
+++ Assignement1/work/miss_s_1E09.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=24
+#PBS -l walltime=00:02:00
+#PBS -q dssc
+#PBS -j oe
+#PBS -N strong_1E09
+
+workdir=${PBS_O_WORKDIR}
+cd ${workdir}
+
+module purge
+module load openmpi/4.0.3/gnu/9.3.0
+
+nbase=1000000000
+for p in 2 6 10 14 18 22
+do
+	/usr/bin/time -f "elapsed: %e\\nuser: %U\\nsystem: %S" mpirun --mca btl '^openib' -np ${p} ../mpi_pi.x ${nbase}
+done
+
+exit 0
