0cb9e96f9efd85cbc6615a76828466b74305f285
lfabris-mhpc <lfabris@sissa.it>
2020-10-31 19:27:56 +0100

    download script

--- Assignement1/download_logs.sh
+++ Assignement1/download_logs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scp lfabris@ct1-005.area.trieste.it:/u/dssc/lfabris/foundations_hpc_fork/Assignement1/work/*.o* ./work/
+scp lfabris@ct1-005.area.trieste.it:/u/dssc/lfabris/foundations_hpc_fork/Assignement1/work_gpu/*.o* ./work_gpu/
 
